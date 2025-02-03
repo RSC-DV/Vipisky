f12028662bb07adac9e7277470a6f3fed5d3aa40
SEBTO-D <d.sebto@ya.ru>
2025-02-03 17:51:35 +1000

    d

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace YourProjectName.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
+    private readonly IWebHostEnvironment _webHostEnvironment;
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
